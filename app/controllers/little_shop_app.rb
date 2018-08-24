e5abbc81115302da3618fd93017d489ad95dd8cc
Steve Thompson <sthompson@SDInstructor1.local>
2018-08-24 12:43:27 -0600

    Added CRUD for merchant with minimal layout.
    This does not have any feature testing. The week 1 content for this was confusing.

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,3 +1,35 @@
 class LittleShopApp < Sinatra::Base
 
+  get '/merchants' do
+    @merchants = Merchant.all
+    set_title("Merchants")
+    erb :"merchants/index"
+  end
+
+  get '/merchants/:id' do |id|
+    @merchant = Merchant.find(id)
+    erb :"merchants/show"
+  end
+
+  get '/merchants/:id/edit' do |id|
+    @merchant = Merchant.find(id)
+    erb :"merchants/edit"
+  end
+
+  put '/merchants/:id' do
+    id = params[:id]
+    Merchant.update(id, params[:merchant])
+    redirect "/merchants/#{id}"
+  end
+
+  delete '/merchants/:id' do |id|
+    Merchant.delete(id)
+    redirect "/merchants"
+  end
+
+  helpers do
+    def set_title(t)
+      @title = t
+    end
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Merchant do
       expect(merchant).to_not be_valid
     end
   end
+
+  describe "Mapping" do
+    it "creates a merchant with a name" do
+      merchant = Merchant.create(name: "First Merchant")
+
+      expect(merchant).to be_valid
+
+      m = Merchant.find(merchant.id)
+      expect(m.id).to equal merchant.id
+    end
+  end
 end
